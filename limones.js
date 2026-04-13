767f69d74a0f876e16c3e73e8bcbcab0314ed3f7
Francisco Rodríguez <panchoxrm@gmail.com>
2026-04-12 22:30:18 -0500

    Agregar colisiones

--- limones.js
+++ limones.js
@@ -1,14 +1,16 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
-let personajeX=canvas.width/2;
-let limonX=canvas.width/2;
-let limonY=5;
 
 const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
 const ALTURA_LIMON=20;
 const ANCHO_LIMON=20;
+
+let personajeX=canvas.width/2;
+let personajeY=canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
+let limonX=canvas.width/2;
+let limonY=0;
 
 function iniciar(){
     dibujarSuelo();
@@ -23,17 +25,19 @@ function dibujarSuelo(){
     
 function dibujarPersonaje(){
     ctx.fillStyle="yellow";
-    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,personajeY,ANCHO_PERSONAJE,ALTURA_PERSONAJE);
 }
 
 function moverIzquierda(){
     personajeX=personajeX-10;
     actualizarPantalla();
+    detectarColision();
     }
 
 function moverDerecha(){
     personajeX=personajeX+10;
     actualizarPantalla();
+    detectarColision();
     }
 
 function actualizarPantalla(){
@@ -56,3 +60,13 @@ function bajarLimon(){
     limonY=limonY+10;
     actualizarPantalla();
     }
+
+function detectarColision(){
+        if (limonX + ANCHO_LIMON > personajeX && 
+        limonX < personajeX + ANCHO_PERSONAJE && 
+        limonY + ALTURA_LIMON > personajeY && 
+        limonY < personajeY + ALTURA_PERSONAJE) {
+        
+        alert("ATRAPADO!!");
+    }
+}
